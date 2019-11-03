177f213495a72a3f653177c1c0f8e9334bd9ac8b
Asim Zahid <asimzahid02@gmail.com>
2019-11-03 09:26:13 -0500

    python files added

---
+++ Counting words/Word \Count.py
@@ -0,0 +1,40 @@
+def count_words(text):
+    """Count how many times each unique word occurs in text."""
+#    counts = dict()  # dictionary of { <word>: <count> } pairs to return
+    
+    # TODO: Convert to lowercase
+    lower_text = text.lower()
+    # TODO: Split text into tokens (words), leaving out punctuation
+    # (Hint: Use regex to split on non-alphanumeric characters)
+    # Cleaning text
+    for char in '-.,\n':
+        text=text.replace(char,' ')
+    word_list = text.split()
+     
+    # TODO: Aggregate word counts using a dictionary
+    counts =    d = {}
+    
+    # counting number of times each word comes up in list of words (in dictionary)
+    for word in word_list: 
+        d[word] = d.get(word, 0) + 1
+    
+    return counts
+
+
+def test_run():
+    with open("input.txt", "r") as f:
+        text = f.read()
+        counts = count_words(text)
+        sorted_counts = sorted(counts.items(), key=lambda pair: pair[1], reverse=True)
+        
+        print("10 most common words:\nWord\tCount")
+        for word, count in sorted_counts[:10]:
+            print("{}\t{}".format(word, count))
+        
+        print("\n10 least common words:\nWord\tCount")
+        for word, count in sorted_counts[-10:]:
+            print("{}\t{}".format(word, count))
+
+
+if __name__ == "__main__":
+    test_run()
